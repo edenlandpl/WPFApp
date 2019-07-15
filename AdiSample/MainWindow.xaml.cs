0e4f6b8c47e5fff2290a5de010de50acd6e74de0
adrianglabik@gmail.com <adrianglabik@gmail.com>
2019-07-15 09:52:34 +0200

    Dodaj pliki projektów.

---
+++ AdiSample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdiSample
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        TabItem _tabUserPage;
+        private void BtnUser1_Click(object sender, RoutedEventArgs e)
+        {
+            MainTab.Items.Clear(); //Clear previous Items in the user controls which is my tabItems
+            var userControls = new MainUserControl1();
+            _tabUserPage = new TabItem { Content = userControls };
+            MainTab.Items.Add(_tabUserPage); // Add User Controls
+            MainTab.Items.Refresh();
+        }
+        private void BtnUser2_Click(object sender, RoutedEventArgs e)
+        {
+            MainTab.Items.Clear(); //Clear previous Items in the user controls which is my tabItems
+            var userControls = new MainUserControl2();
+            _tabUserPage = new TabItem { Content = userControls };
+            MainTab.Items.Add(_tabUserPage); // Add User Controls
+            MainTab.Items.Refresh();
+        }
+        private void BtnUser3_Click(object sender, RoutedEventArgs e)
+        {
+            MainTab.Items.Clear(); //Clear previous Items in the user controls which is my tabItems
+            var userControls = new MainUserControl3();
+            _tabUserPage = new TabItem { Content = userControls };
+            MainTab.Items.Add(_tabUserPage); // Add User Controls
+            MainTab.Items.Refresh();
+        }
+    }
+}
