ba80c46765138a6e70036d790d8fe6157c57f0d9
Adrian Głąbik <46246127+edenlandpl@users.noreply.github.com>
2019-09-13 14:46:08 +0200

    Add files via upload

---
+++ AdiSample/UserControls/UserControl03/UC03_19_ToolBar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdiSample.UserControls.UserControl03
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UC03_19_ToolBar.xaml
+    /// </summary>
+    public partial class UC03_19_ToolBar : UserControl
+    {
+        public UC03_19_ToolBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AdiSample/UserControls/UserControl03/UC03_21_ProgressBar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdiSample.UserControls.UserControl03
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UC03_21_ProgressBar.xaml
+    /// </summary>
+    public partial class UC03_21_ProgressBar : UserControl
+    {
+        public UC03_21_ProgressBar()
+        {
+            InitializeComponent();
+        }
+        //private void Window_ContentRendered(object sender, EventArgs e)
+        //{
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        pbStatus.Value++;
+        //        Thread.Sleep(100);
+        //    }
+        //}
+
+        private void PbButton_Click(object sender, RoutedEventArgs e)
+        {
+            pbStatus.Value++;
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    pbStatus.Value++;
+            //    //Thread.Sleep(100);
+            //}
+        }
+    }
+}
